2492f6edd866610d82f1083b824a003d363650fd
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2023-07-06 16:02:51 +0300

    32. Pagination output. Pagination, 33. State Change Mechanism Explained

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -2,7 +2,6 @@ import {useMemo} from 'react'
 
 export const useSortedPosts = (posts, sort) => {
 	const sortedPosts = useMemo(() => {
-		console.log('Sorted posts function has executed')
 		if(sort) {
 			return [...posts].sort((a, b) => a[sort].localeCompare(b[sort]))
 		}
